68490def55632e4a748c4120845dd9cb5bc7b974
Shakib KATAYA <kataya.shakib@gmail.com>
2023-05-02 18:57:46 +0300

    Install dotenv & edit hardhat.config.js

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const INFURA_HTTP_URL = process.env.INFURA_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: INFURA_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
